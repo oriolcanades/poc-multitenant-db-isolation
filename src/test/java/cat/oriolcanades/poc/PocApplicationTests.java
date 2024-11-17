7d6d857a1d924c313421e682ff75f8d440e0458a
Oriol Canadés <oriol.canades@in2.es>
2024-11-17 13:07:44 +0100

    Initial commit

---
+++ src/test/java/cat/oriolcanades/poc/PocApplicationTests.java
@@ -0,0 +1,14 @@
+package cat.oriolcanades.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test is empty because it is only used to check that the context loads correctly
+	}
+
+}
